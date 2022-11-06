28cbf2616bbb29dba1c97eb49ab42dd9bee96f95
Hussam Rostum <hussam.rostum@gmail.com>
2022-11-06 02:49:22 +0100

    function..8..

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,10 +7,12 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
+    
     """Test the calc module."""
 
     def test_add_numbers(self):
-        """Test adding numbers together."""
-        result = calc.add(5, 6)
 
-        self.assertEqual(result, 11)
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
