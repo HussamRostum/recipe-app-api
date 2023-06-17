e237d901c1dfb3159123360d83c3f4908bb0edc8
Hussam Rostum <hussam.rostum@gmail.com>
2023-06-17 22:24:32 +0200

    configure the database in django

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(10, 6)
+
+        self.assertEqual(res, 16)
+
+    def test_sub_numbers(self):
+        """Test adding numbers together."""
+        res = calc.sub(10, 15)
+        self.assertEqual(res, 5)
